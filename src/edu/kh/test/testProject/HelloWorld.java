f655ebe869ed7f1c1ebd510e75a571ae8d166e1e
garam <dheotjd87@gmail.com>
2022-12-06 10:40:23 +0900

    First Commit

---
+++ src/edu/kh/test/testProject/HelloWorld.java
@@ -0,0 +1,16 @@
+package edu.kh.test.testProject;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int a = 10;
+		int b = 20;
+		System.out.println(a + b);
+		String c = "오가람";
+		String d = "kh정보교육원";
+		System.out.println(c + " " + d);
+		System.out.println("메롱 메롱 메롱 메롱 메롱");
+		System.out.println("1 + 2");
+		System.out.println("14 * 19 = " + 266);
+	}
+}
